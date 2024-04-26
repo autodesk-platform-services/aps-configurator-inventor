cd9613031b4d5889118058874b3e9de9af3e9428
Adam Nagy <adam_e_nagy@hotmail.com>
2024-04-26 16:02:45 +0100

    Switch to latest version of Viewer

--- WebApplication/ClientApp/src/components/shared.js
+++ WebApplication/ClientApp/src/components/shared.js
@@ -17,7 +17,7 @@
 /////////////////////////////////////////////////////////////////////
 
 // use fixed version of the viewer to avoid usage of untested viewer version
-const viewerVersion = '7.29.0';
+const viewerVersion = '7.96.0';
 
 // in case you need to debug Viewer script/css - remove '.min' from the URLs
 export const viewerCss = `https://developer.api.autodesk.com/modelderivative/v2/viewers/${viewerVersion}/style.min.css`;
