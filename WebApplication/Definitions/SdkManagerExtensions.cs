796136b1e85a458c9e00b17f1dfe6b20add0ff04
Radek Novotny <radek.novotny@autodesk.com>
2025-06-06 13:23:16 +0200

    Replace authentication http requests by SDK calls

---
+++ WebApplication/Definitions/SdkManagerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.SDKManager;
+
+namespace WebApplication.Definitions
+{
+    public static class SdkManagerExtensions
+    {
+        public static void SetEnvFromAuthAddress(this SDKManager sdkManager, string authAddress)
+        {
+            sdkManager.ApsConfiguration = new ApsConfiguration(
+                  authAddress.Contains("-dev") ? AdskEnvironment.Dev : (
+                  authAddress.Contains("-stg") ? AdskEnvironment.Stg : AdskEnvironment.Prd
+            ));
+        }
+    }
+}
--- WebApplication/Services/TokenService.cs
+++ WebApplication/Services/TokenService.cs
@@ -1,20 +1,22 @@
 ﻿using Autodesk.Forge.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
-using static WebApplication.Services.ProfileProvider;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
-using System.Text.Json.Serialization;
 using System.Net.Http;
-using WebApplication.Middleware;
 using Microsoft.AspNetCore.Http;
 using System.Web;
-using System.Text.Json;
 using System.Collections.Concurrent;
 using WebApplication.Utilities;
 using System.Linq;
 using System.Collections;
+using Autodesk.Authentication;
+using Autodesk.SDKManager;
+using Autodesk.Authentication.Model;
+using System.Text;
+using Autodesk.Authentication.Client;
+using WebApplication.Definitions;
 
 namespace WebApplication.Services
 {
@@ -27,27 +29,14 @@ namespace WebApplication.Services
         private readonly IHttpContextAccessor _httpContextAccessor;
 
         // store PKCE code to token pair
-        private ConcurrentDictionary<string, KeyValuePair<DateTime, Lazy<Task<TokenJson>>>> codeToToken = new ConcurrentDictionary<string, KeyValuePair<DateTime, Lazy<Task<TokenJson>>>>();
-        
+        private ConcurrentDictionary<string, KeyValuePair<DateTime, Lazy<Task<ThreeLeggedToken>>>> codeToToken = new ConcurrentDictionary<string, KeyValuePair<DateTime, Lazy<Task<ThreeLeggedToken>>>>();
+
         // store loginID to code_verifier, to be able to use correct stored 'code_verifier' per login
         public ConcurrentDictionary<string, string> id2Verifier = new ConcurrentDictionary<string, string>();
 
-        public class TokenJson
-        {
-            [JsonPropertyName("access_token")]
-            public string AccessToken { get; set; }
-
-            [JsonPropertyName("token_type")]
-            public string TokenType { get; set; }
-
-            [JsonPropertyName("expires_in")]
-            public long ExpiresIn { get; set; }
-
-            [JsonPropertyName("refresh_token")]
-            public string RefreshToken { get; set; }
-        }
-
         public ForgeConfiguration Configuration { get; }
+
+        private AuthenticationClient authenticationClient;
 
         public TokenService(IOptions<ForgeConfiguration> forgeConfiguration, IOptions<ForgeConfiguration> optionsAccessor,
             IHttpClientFactory clientFactory, IHttpContextAccessor httpContextAccessor)
@@ -57,6 +46,13 @@ namespace WebApplication.Services
 
             _clientFactory = clientFactory;
             _httpContextAccessor = httpContextAccessor;
+
+            var sdkManager = SdkManagerBuilder.Create().Build();
+
+            // Set environment from AuthenticationAddress
+            sdkManager.SetEnvFromAuthAddress(Configuration.AuthenticationAddress.AbsolutePath);
+
+            authenticationClient = new AuthenticationClient(sdkManager);
         }
 
         public async Task<string> GetToken(string code, string verifierId)
@@ -65,9 +61,9 @@ namespace WebApplication.Services
                 return "";
 
             if (!codeToToken.ContainsKey(code))
-                codeToToken.TryAdd(code, KeyValuePair.Create(DateTime.Now, new Lazy<Task<TokenJson>>(async () => await GetTokenFromCode(code, verifierId))));
+                codeToToken.TryAdd(code, KeyValuePair.Create(DateTime.Now, new Lazy<Task<ThreeLeggedToken>>(async () => await GetTokenFromCode(code, verifierId))));
 
-            Lazy<Task<TokenJson>> lazy = codeToToken[code].Value;
+            Lazy<Task<ThreeLeggedToken>> lazy = codeToToken[code].Value;
             var jo = await lazy.Value;
 
             return jo.AccessToken;
@@ -88,41 +84,26 @@ namespace WebApplication.Services
             return $"{scheme}{Uri.SchemeDelimiter}{context.Request.Host}";
         }
 
-        private async Task<TokenJson> GetTokenFromCode(string code, string verifierId)
+        private async Task<ThreeLeggedToken> GetTokenFromCode(string code, string verifierId)
         {
-            HttpContext context = _httpContextAccessor.HttpContext;
-
-            var client = _clientFactory.CreateClient();
-            string baseUrl = Configuration.AuthenticationAddress.GetLeftPart(System.UriPartial.Authority);
-            var url = $"{baseUrl}/authentication/v2/token";
-
             string codeVerifier = id2Verifier[verifierId];
             var callbackUrl = GetCallbackUrl();
-            var body = new System.Net.Http.FormUrlEncodedContent(
-                 new Dictionary<string, string>
-                 {
-                     { "grant_type", "authorization_code" },
-                     { "code", code},
-                     { "client_id", Configuration.ClientId},
-                     { "client_secret", Configuration.ClientSecret},
-                     { "code_verifier", codeVerifier },
-                     { "redirect_uri", $"{callbackUrl}" }
-                 }
-             );
 
             CleanupExpiredTokens();
 
-            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var encodedHost = HttpUtility.UrlEncode(callbackUrl);
+            string authorization = Convert.ToBase64String(Encoding.UTF8.GetBytes(Configuration.ClientId + ":" + Configuration.ClientSecret));
 
-            HttpResponseMessage response = await client.PostAsync(url, body);
+            var response = await authenticationClient.tokenApi.FetchTokenAsync(authorization, GrantType.AuthorizationCode, code, callbackUrl, codeVerifier, throwOnError: false);
             if (response.IsSuccessStatusCode)
             {
-                string responseStr = await response.Content.ReadAsStringAsync();
-                var json = JsonSerializer.Deserialize<TokenJson>(responseStr);
-                return json;
+                return await LocalMarshalling.DeserializeAsync<ThreeLeggedToken>(response.Content);
             }
 
             return null;
+
+            // This function is currently unusable because it ignores codeVerifier
+            // return await authenticationClient.GetThreeLeggedTokenAsync(Configuration.ClientId, code, callbackUrl, Configuration.ClientSecret, codeVerifier);
         }
 
         private void CleanupExpiredTokens()
@@ -136,7 +117,7 @@ namespace WebApplication.Services
                 var tokenJson = item.Value.Value.Value.Result;
                 var ExpiresIn = tokenJson.ExpiresIn;
 
-                var tokenExpTime = tokenTime.AddSeconds(ExpiresIn);
+                var tokenExpTime = tokenTime.AddSeconds((double)ExpiresIn);
                 if (DateTime.Now > tokenExpTime)
                     codesForDelete.Add(item);
             }
