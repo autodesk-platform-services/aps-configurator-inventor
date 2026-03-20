c69850e619cec01004bbffd113c1bc2d2cf1eda4
Pavel Urx <pavel.urx@autodesk.com>
2026-03-20 12:25:42 +0100

    implemented check if package-lock.json does not contain autodesk registries

--- cicd/build.ps1
+++ cicd/build.ps1
@@ -1,5 +1,8 @@
 . ./cicd/common.ps1
 
+& node (Join-Path $PSScriptRoot "validate-package-lock-registry.js")
+CheckLastExitCode
+
 Write-Output "**** Building ****"
 msbuild -restore /property:Configuration=Release
 CheckLastExitCode
