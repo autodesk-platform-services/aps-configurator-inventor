41a2978de6104b4e5250035557347f9de96bce8a
Pavel Urx <pavel.urx@autodesk.com>
2026-03-20 12:57:45 +0100

    moved npm registry test to pipeline repository

--- cicd/build.ps1
+++ cicd/build.ps1
@@ -1,8 +1,5 @@
 . ./cicd/common.ps1
 
-& node (Join-Path $PSScriptRoot "validate-package-lock-registry.js")
-CheckLastExitCode
-
 Write-Output "**** Building ****"
 msbuild -restore /property:Configuration=Release
 CheckLastExitCode
